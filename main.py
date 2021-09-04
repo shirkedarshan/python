3662934a6d82b6b9d21036ffc40a071128d96b3a
Darshan Shirke <shirkedarshan@gmail.com>
2021-09-04 21:17:24 +0530

    Dict Basics

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'Comp Science']}
+print(student)
+print(student['courses'])
+print(student.get('name', 'DATA NOT FOUND'))
+student['name'] = 'Darshan'
+print(student.get('name', 'DATA NOT FOUND'))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+del student['age']
+print(student.keys())
+print(student.values())
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for key, value in student.items():
+    print(key, value)
