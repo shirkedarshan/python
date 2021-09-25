29fadb20bf17c9ef7c9ea81ecf62d26cf1a6c9b1
Darshan Shirke <shirkedarshan@gmail.com>
2021-09-25 18:56:31 +0530

    implemented

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    f = open('testfile.txt')
+    f = oo
+    # if f.name == 'testfile.txt':
+    #     raise Exception
+    pass
+except FileNotFoundError:
+    print('Sorry, This file does not exist')
+    pass
+except Exception:
+    print('Something went wrong')
+else:
+    print(f.read())
+    f.close()
+    pass
+finally:
+    print("Finally block huh")
+    pass
